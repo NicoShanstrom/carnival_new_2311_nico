63e962f891d5e6a7d6b6a6f3493a432189d80a42
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-09 11:32:40 -0500

    Create test for show visitor preferences and add preferences

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -2,16 +2,26 @@ require 'spec_helper'
 require './lib/visitor'
 
 RSpec.describe Visitor do
-    it 'initializes with attributes' do
+    it 'initializes visitors with attributes' do
         visitor1 = Visitor.new('Bruce', 54, '$10')
+        visitor2 = Visitor.new('Tucker', 36, '$5')
+        visitor3 = Visitor.new('Penny', 64, '$15')
+
         expect(visitor1.name).to eq("Bruce")
         expect(visitor1.height).to eq(54)
         expect(visitor1.spending_money).to eq('$10')
     end
 
-    xit 'can show visitor preferences' do
+    it 'can show visitor preferences and add preferences' do
         visitor1 = Visitor.new('Bruce', 54, '$10')
+        visitor2 = Visitor.new('Tucker', 36, '$5')
+        visitor3 = Visitor.new('Penny', 64, '$15')
+        
         expect(visitor1.preferences).to eq([])
+        visitor1.add_preference(:gentle)
+        visitor1.add_preference(:thrilling)
+        expect(visitor1.preferences).to eq([:gentle, :thrilling])
+
     end
 end
 
