ab50c452a7d3cd82a3a0f4a297f5a04c47c8c20d
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-09 11:26:45 -0500

    Fix type in spending_money expect

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -3,11 +3,16 @@ require './lib/visitor'
 
 RSpec.describe Visitor do
     it 'initializes with attributes' do
-        visitor = Visitor.new('Bruce', 54, '$10')
-        expect(visitor.name).to eq("Bruce")
-        expect(visitor.height).to eq(54))
-        expect(visitor.spending_money).to eq(10)
+        visitor1 = Visitor.new('Bruce', 54, '$10')
+        expect(visitor1.name).to eq("Bruce")
+        expect(visitor1.height).to eq(54)
+        expect(visitor1.spending_money).to eq('$10')
     end
-        
+
+    xit 'can show visitor preferences' do
+        visitor1 = Visitor.new('Bruce', 54, '$10')
+        expect(visitor1.preferences).to eq([])
+    end
+end
 
 
