316b5625aa35937182aaa570cef72544b85f4b1f
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-09 13:58:58 -0500

    Try to make tests and methods based off interaction pattern, FAIL

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -12,8 +12,17 @@ RSpec.describe Ride do
 
     it 'can collect revenue from rides' do
         ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+        visitor1 = Visitor.new('Bruce', 54, '$10')
+        visitor2 = Visitor.new('Tucker', 36, '$5')
 
         expect(ride1.total_revenue).to eq(0)
+        
+        ride1.board_rider(visitor1)
+        ride1.board_rider(visitor2)
+        ride1.board_rider(visitor1)
+
+        expect(ride1.total_revenue).to eq(3)
+
     end
 
     it 'allows visitors to board ride if their preferences match ride excitement level and keep a log' do
@@ -31,4 +40,16 @@ RSpec.describe Ride do
         expect(ride1.rider_log).to include({ visitor1 => 2, visitor2 => 1 })
     end
 
+    it 'can deduct visitor spending money after using it for rides' do
+        ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+        visitor1 = Visitor.new('Bruce', 54, '$10')
+        visitor2 = Visitor.new('Tucker', 36, '$5')
+
+        ride1.board_rider(visitor1)
+        ride1.board_rider(visitor2)
+        ride1.board_rider(visitor1)
+
+        expect(visitor1.spending_money.to_i).to eq(8)
+        expect(visitor2.spending_money.to_i).to eq(4)
+    end
 end
