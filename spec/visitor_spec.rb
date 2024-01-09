1effbc36a5051b43f5f2c956e631d56ec9d1e021
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-09 11:10:44 -0500

    Make test for visitor attributes

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,2 +1,13 @@
 require 'spec_helper'
+require './lib/visitor'
+
+RSpec.describe Visitor do
+    it 'initializes with attributes' do
+        visitor = Visitor.new('Bruce', 54, '$10')
+        expect(visitor.name).to eq("Bruce")
+        expect(visitor.height).to eq(54))
+        expect(visitor.spending_money).to eq(10)
+    end
+        
+
 
