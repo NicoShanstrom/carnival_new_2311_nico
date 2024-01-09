1ff672e747e2c50b5e6b7dfc30a3f418aabab163
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-09 13:25:25 -0500

    create a #board_rider and #rider_log to pass tests

--- lib/ride.rb
+++ lib/ride.rb
@@ -2,12 +2,29 @@ class Ride
     attr_reader :name,
                 :min_height,
                 :admission_fee,
-                :excitement
+                :excitement,
+                :rider_log
+                
 
     def initialize(attributes)
         @name = attributes[:name]
         @min_height = attributes[:min_height]
         @admission_fee = attributes[:admission_fee]
         @excitement = attributes[:excitement]
+        @total_revenue = 0
+        @rider_log = {}
+    end
+
+    def total_revenue
+        @total_revenue
+    end
+
+    def board_rider(visitor)
+        if visitor.preferences.include?(@excitement) && visitor.tall_enough?(@min_height)
+        @rider_log[visitor] ||= 0
+        @rider_log[visitor] += 1
+        else
+            raise "Visitor preferences or height do not match ride requirements"
+        end
     end
 end
