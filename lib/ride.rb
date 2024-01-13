5e4a28b72d068b0bb6e42c9bb10e193331d06d23
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-13 18:07:01 -0500

    Created #board_rider which boards riders, holds a hash log, and deducts visitor spending money based off ride admission

--- lib/ride.rb
+++ lib/ride.rb
@@ -3,38 +3,47 @@ class Ride
                 :min_height,
                 :admission_fee,
                 :excitement,
-                :rider_log,
+                :rider_log
                 
 
-    def initialize(attributes)
-        @name = attributes[:name]
-        @min_height = attributes[:min_height]
-        @admission_fee = attributes[:admission_fee]
-        @excitement = attributes[:excitement]
-        @total_revenue = 0
-        @rider_log = {}
-       
+    def initialize(info)
         
+        @name = info[:name]
+        @min_height = info[:min_height]
+        @admission_fee = info[:admission_fee]
+        @excitement = info[:excitement]
+        @rider_log = {}
     end
 
     def total_revenue
-        @total_revenue += admission_fee
+        @rider_log.values.sum * @admission_fee
     end
+    
+    # def board_rider(visitor)
+        
+    #     if visitor.preferences.include?(@excitement) && visitor.tall_enough?(@min_height)
+    #         if @rider_log[visitor].nil?
+    #             @rider_log[visitor] = 0
+    #             @rider_log[visitor] += 1
+    #         else
+    #             @rider_log[visitor] += 1
+               
+    #         end
+    #         visitor.spending_money -= admission_fee
+    #     else
+    #         puts "Visitor preferences or height do not match ride requirements"
+    #     end
+    # end
 
     def board_rider(visitor)
         if visitor.preferences.include?(@excitement) && visitor.tall_enough?(@min_height)
-        @rider_log[visitor] ||= 0
-        @rider_log[visitor] += 1
-        visitor.deduct_money(admission_fee)
+            @rider_log[visitor] ||= 0  # Set default value if the key doesn't exist
+            @rider_log[visitor] += 1
+            visitor.spending_money -= admission_fee
         else
-            puts "Visitor preferences or height do not match ride requirements"
+        puts "Visitor preferences or height do not match ride requirements"
         end
     end
 
-    private 
-
-    def deduct_money(admission_fee)
-        visitor.deduct_money(admission_fee)
-    end
 
 end
