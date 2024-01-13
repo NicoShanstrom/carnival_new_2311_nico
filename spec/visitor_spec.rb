cd4c96ade8de58825058fd8d689d25681d518e54
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-13 18:04:31 -0500

    Deleted lines that are not necessary

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -4,18 +4,16 @@ require './lib/visitor'
 RSpec.describe Visitor do
     it 'initializes visitors with attributes' do
         visitor1 = Visitor.new('Bruce', 54, '$10')
-        visitor2 = Visitor.new('Tucker', 36, '$5')
-        visitor3 = Visitor.new('Penny', 64, '$15')
+     
 
         expect(visitor1.name).to eq("Bruce")
         expect(visitor1.height).to eq(54)
-        expect(visitor1.spending_money).to eq('$10')
+        expect(visitor1.spending_money).to eq(10)
     end
 
     it 'can show visitor preferences and add preferences' do
         visitor1 = Visitor.new('Bruce', 54, '$10')
-        visitor2 = Visitor.new('Tucker', 36, '$5')
-        visitor3 = Visitor.new('Penny', 64, '$15')
+ 
 
         expect(visitor1.preferences).to eq([])
         visitor1.add_preference(:gentle)
