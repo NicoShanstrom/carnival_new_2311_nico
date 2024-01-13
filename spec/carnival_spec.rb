9ce003a3431204f820726632e4363a553dcff756
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-13 18:28:02 -0500

    Add initialize test for carnival class

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+require './lib/ride'
+require './lib/visitor'
+require './lib/carnival'
+
+RSpec.describe Carnival do
+    it 'can be a carnival with an atrribute of duration' do
+        
+        carnival = Carnival.new('14 days')
+
+        expect(carnival.duration).to eq('14 days')
+    end
+end
